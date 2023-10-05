01708cf813ba81cc2305f7407e7fe1e213eca2b7
joshuaoweipadei <oweipadeijoshie@gmail.com>
2023-10-05 03:32:59 -0400

    setting up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
 
       },
       backgroundImage: {
-        "hero-pattern": "url('/images/herobg2.png')",
+        "hero-pattern": "url('/images/herobg.png')",
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
